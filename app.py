9e9b244597114f56dec1e35cf223f11798e25339
AraujoDev <silvabaraujo2007@gmail.com>
2026-08-23 23:20:08 -0300

    Banco de Dados SQlite - init

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect("blog.db")
+    conn.row_factory = sqlite3.Row
+
+    return conn
+
+
+def init_db():
+    conn = get_db_connection()
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
 
 @app.route("/")
 def home():
@@ -8,7 +31,8 @@ def home():
 
 @app.route("/post/<int:id>")
 def post(id):
-    return render_template("post.html")
+    return render_template("post.html", post_id=id)
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
