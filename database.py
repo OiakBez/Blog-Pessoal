5e5b2bfdaeda9cc30ad5f9f11feb2a7ca7ae3f08
Araujo Dev <silvabaraujo2007@gmail.com>
2026-09-04 13:07:23 -0300

    refactor: separar operações de banco em database.py

--- database.py
+++ database.py
@@ -47,3 +47,58 @@ def create_admin():
         conn.commit()
 
     conn.close()
+
+def get_all_posts():
+    conn = get_db_connection()
+
+    posts = conn.execute(
+        "SELECT * FROM posts ORDER BY id DESC"
+    ).fetchall()
+
+    conn.close()
+    return posts
+
+def get_post(post_id):
+    conn = get_db_connection()
+
+    post = conn.execute(
+        "SELECT * FROM posts WHERE id = ?",
+        (post_id,)
+    ).fetchone()
+
+    conn.close()
+
+    return post
+
+def add_post(title, content):
+    conn = get_db_connection()
+
+    cursor = conn.execute(
+        "INSERT INTO posts (title, content) VALUES (?, ?)",
+        (title, content)
+    )
+
+    conn.commit()
+    conn.close()
+
+def update_post(post_id, title, content):
+    conn = get_db_connection()
+
+    conn.execute(
+        "UPDATE posts SET title = ?, content = ? WHERE id = ?",
+        (title, content, post_id)
+    )
+
+    conn.commit()
+    conn.close()
+
+def delete_post(post_id):
+    conn = get_db_connection()
+
+    conn.execute(
+        "DELETE FROM posts WHERE id = ?",
+        (post_id,)
+    )
+
+    conn.commit()
+    conn.close()
