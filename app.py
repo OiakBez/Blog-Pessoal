d9cab85626ed0ab4e0a39e2ccc49feefffdbaf4b
AraujoDev <silvabaraujo2007@gmail.com>
2026-09-09 12:57:27 -0300

    fix: inicializar banco e admin em producao

--- app.py
+++ app.py
@@ -177,7 +177,8 @@ def logout():
 def page_not_found(error):
     return render_template("404.html"), 404
 
+init_db()
+create_admin()
+
 if __name__ == "__main__":
-    init_db()
-    create_admin()
     app.run(debug=False)
